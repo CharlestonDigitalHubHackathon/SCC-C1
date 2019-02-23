953b70967b3c4f3a3e62d4a9e1a36653c2d4c4ee
Chandler DeLoach <deloachcd@g.cofc.edu>
2019-02-23 11:37:54 -0500

    Wrote basic model

--- cc1hackathon/cc1app/models.py
+++ cc1hackathon/cc1app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class AirPollutionRecords(models.Model):
+    element_name = models.CharField(max_length=50)
+    year = models.IntegerField()
+    unit_of_measure = models.CharField(max_length=50)
+    record_value = models.IntegerField()
+    metro_area = models.CharField(max_length=100)
