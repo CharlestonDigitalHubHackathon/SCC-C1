c70f99e2cf5d318d2cc7e7eed827e5f07cf278a2
Chandler DeLoach <deloachcd@g.cofc.edu>
2019-02-23 13:44:39 -0500

    Updated models.py and generate_model_loader.py

--- cc1hackathon/cc1app/models.py
+++ cc1hackathon/cc1app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class AirPollutionRecords(models.Model):
-    element_name = models.CharField(max_length=50)
-    year = models.IntegerField()
-    unit_of_measure = models.CharField(max_length=50)
-    record_value = models.IntegerField()
-    metro_area = models.CharField(max_length=100)
+    parameter_name = models.CharField(max_length=50)
+    date_local = models.IntegerField()
+    units_of_measure = models.CharField(max_length=50)
+    arithmetic_mean = models.IntegerField()
+    cbsa_name = models.CharField(max_length=100)
--- cc1hackathon/generate_model_loader.py
+++ cc1hackathon/generate_model_loader.py
@@ -2,6 +2,8 @@ import sys
 
 import pandas as pd
 
+from cc1app.models import AirPollutionRecords
+
 
 def load_array_from_filename(filename):
     # ARRAY FORMAT:
@@ -14,8 +16,16 @@ def load_array_from_filename(filename):
 
 def main():
     records = load_array_from_filename(sys.argv[1])
-    for record in records:
-        print(records)
+    # 
+    dataframe_fields = list(records)
+    for index, row in records.iterrows():
+        dataframe_values = {}
+        for 
+        command = 'e{} = AirPollutionRecords('.format(index)
+        for field in dataframe_fields:
+            command += "{}='{}',".format(field, row[field])
+        command += ')\ne{}.save()'.format(index)
+        print(command)
 
 
 if __name__ == "__main__":
