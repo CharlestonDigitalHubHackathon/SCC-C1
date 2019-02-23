e84f3fc75f11945b59eb2ee06d7310ed85ed9b7d
AustinHunt353 <huntaj@g.cofc.edu>
2019-02-23 09:58:08 -0500

    new front end

--- cc1hackathon/cc1app/views.py
+++ cc1hackathon/cc1app/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
+from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.contrib.auth import logout
+# default user table
+
+# for forgot password feature
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template import loader
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+
+from django.contrib.auth.models import User
+
+# import the models
+from .models import *
+# use this function for returning json data on ajax requests
+import json
+
+# for ajax requests, returning JSON to JS
+def render_to_json_response(context, **response_kwargs):
+    data = json.dumps(context)
+    response_kwargs['content_type'] = 'application/json'
+    return HttpResponse(data, **response_kwargs)
+
+
+# home page function
+@csrf_exempt
+def index(request):
+    template = loader.get_template('main/home_admin.html')
+    context = {'':''}
+    return HttpResponse(template.render(context, request))
+
+# 404 page
+@csrf_exempt
+def err404(request):
+    template = loader.get_template('main/404.html')
+    context = {
+        '': '',
+    }
+    return HttpResponse(template.render(context, request))
+
+
